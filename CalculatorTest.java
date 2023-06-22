fb6f35fc1aca67b09753e9ef731314e0c55782ad
Sreevaishnavisadhasivam <91306123+Sreevaishnavisadhasivam@users.noreply.github.com>
2023-06-22 17:38:00 +0530

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+    
+    @Test
+    public void testSubtraction() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
+    }
+    
+    @Test
+    public void testMultiplication() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(4, 3);
+        assertEquals(12, result);
+    }
+    
+    @Test
+    public void testDivision() {
+        Calculator calculator = new Calculator();
+        double result = calculator.divide(10, 2);
+        assertEquals(5.0, result, 0.0001);
+    }
+}
